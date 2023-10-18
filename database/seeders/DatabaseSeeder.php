62ca7294a59a76b1bae87c91c1bc881e81f72731
kammysakulove <kammysakulove@gmail.com>
2023-10-18 17:47:37 +0900

    [add] SPA認証の追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Database\Seeders;
+  namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
+  use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+  class DatabaseSeeder extends Seeder
+  {
     /**
      * Seed the application's database.
      *
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+      $this->call([
+        UserSeeder::class,
+      ]);
     }
-}
+  }
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,26 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+  use App\Http\Controllers\AuthController;
+  use App\Models\User;
+  use Illuminate\Http\Request;
+  use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+  /*
+  |--------------------------------------------------------------------------
+  | API Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register API routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | is assigned the "api" middleware group. Enjoy building your API!
+  |
+  */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+  Route::middleware('auth:sanctum')->get('users', function (Request $request) {
+    return User::all();
+  });
+  Route::post('login', [AuthController::class, 'login']);
+  Route::post('logout', [AuthController::class, 'logout']);
+//  Route::get('users', function () {
+//    return User::all();
+//  });
