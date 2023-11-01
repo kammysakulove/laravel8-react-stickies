d226a40bc3a81117d7a8c57a23aad068a1b79401
kammysakulove <kammysakulove@gmail.com>
2023-11-01 15:03:10 +0900

    [add] エラー表示の追記 #1

--- resources/ts/features/auth/routes/Login.tsx
+++ resources/ts/features/auth/routes/Login.tsx
@@ -1,4 +1,13 @@
-import { Button, Flex, Input } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Alert,
+  AlertIcon,
+} from "@chakra-ui/react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useLogin, LoginCredentials } from "../api/login";
 import { useNavigate } from "react-router-dom";
@@ -21,20 +30,30 @@ const Login = () => {
       <Flex height="100vh" alignItems="center" justifyContent="center">
         <Flex direction="column" background="gray.50" p={12} rounded={6}>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <Input
-              {...register("email")}
-              placeholder="user@test.jp"
-              variant="outline"
-              mb={3}
-              type="email"
-            />
-            <Input
-              {...register("password")}
-              placeholder="*******"
-              variant="outline"
-              mb={6}
-              type="password"
-            />
+            <FormControl mb={3} isInvalid={false}>
+              <FormLabel>Email</FormLabel>
+              <Input
+                {...register("email")}
+                placeholder="user@test.jp"
+                variant="outline"
+                type="email"
+              />
+              <FormErrorMessage>test</FormErrorMessage>
+            </FormControl>
+            <FormControl mb={6} isInvalid={false}>
+              <FormLabel>パスワード</FormLabel>
+              <Input
+                {...register("password")}
+                placeholder="*******"
+                variant="outline"
+                type="password"
+              />
+              <FormErrorMessage>test</FormErrorMessage>
+            </FormControl>
+            <Alert mb={5} status="error">
+              <AlertIcon />
+              Your Chakra experience may be degraded.
+            </Alert>
             <Button colorScheme="teal" type="submit">
               ログイン
             </Button>
