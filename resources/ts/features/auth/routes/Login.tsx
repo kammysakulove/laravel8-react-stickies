353928f1e0715a008dcb78890175f2aad7ecea7a
kammysakulove <kammysakulove@gmail.com>
2023-11-06 16:11:45 +0900

    [add] テキストフィールドをコンポーネント化 #3

--- resources/ts/features/auth/routes/Login.tsx
+++ resources/ts/features/auth/routes/Login.tsx
@@ -1,19 +1,11 @@
-import {
-  Button,
-  Flex,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Input,
-  Alert,
-  AlertIcon,
-} from "@chakra-ui/react";
+import { Button, Flex, Alert, AlertIcon } from "@chakra-ui/react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useLogin, LoginCredentials } from "../api/login";
 import { useNavigate } from "react-router-dom";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
+import { InputField } from "@/components/Form/InputField";
 
 const schema = z.object({
   email: z.string().min(1, "入力してください"),
@@ -49,30 +41,22 @@ const Login = () => {
       <Flex height="100vh" alignItems="center" justifyContent="center">
         <Flex direction="column" background="gray.50" p={12} rounded={6}>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <FormControl mb={3} isInvalid={!!errors.email}>
-              <FormLabel>Email</FormLabel>
-              <Input
-                {...register("email")}
-                placeholder="user@test.jp"
-                variant="outline"
-                type="email"
-              />
-              <FormErrorMessage>
-                {errors.email && errors.email.message}
-              </FormErrorMessage>
-            </FormControl>
-            <FormControl mb={6} isInvalid={!!errors.password}>
-              <FormLabel>パスワード</FormLabel>
-              <Input
-                {...register("password")}
-                placeholder="*******"
-                variant="outline"
-                type="password"
-              />
-              <FormErrorMessage>
-                {errors.password && errors.password.message}
-              </FormErrorMessage>
-            </FormControl>
+            <InputField
+              mb={3}
+              label="Email"
+              placeholder="user@test.jp"
+              error={errors.email}
+              type="email"
+              register={register("email")}
+            />
+            <InputField
+              mb={3}
+              label="パスワード"
+              placeholder="*******"
+              error={errors.password}
+              type="password"
+              register={register("password")}
+            />
             {authError && (
               <Alert mb={5} status="error">
                 <AlertIcon />
