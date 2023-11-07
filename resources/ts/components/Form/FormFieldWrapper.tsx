847c8a2ef66c9eb447183a1ffae8171bbfd77c97
kammysakulove <kammysakulove@gmail.com>
2023-11-08 00:37:57 +0900

    [add] #7 コンポーネントの追加
    LoginForm,RegisterFormの追加とそれにともなう変更

--- resources/ts/components/Form/FormFieldWrapper.tsx
+++ resources/ts/components/Form/FormFieldWrapper.tsx
@@ -3,16 +3,15 @@ import { FieldError } from "react-hook-form";
 import { ReactNode } from "react";
 
 export type FormFieldWrapperProps = {
-  mb: number;
   label: string;
   error: FieldError | undefined;
   children: ReactNode;
 };
 
 export const FormFieldWrapper = (props: FormFieldWrapperProps) => {
-  const { mb, label, error, children } = props;
+  const { label, error, children } = props;
   return (
-    <FormControl mb={mb} isInvalid={!!error}>
+    <FormControl isInvalid={!!error}>
       <FormLabel>{label}</FormLabel>
       {children}
       <FormErrorMessage>{error && error.message}</FormErrorMessage>
