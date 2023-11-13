a71931ceb5ac5763eb0b1c5f90d6d8cd3329145d
kammysakulove <kammysakulove@gmail.com>
2023-11-13 15:34:41 +0900

    [update] #7 ユーザー登録APIコンポーネントの修正

--- resources/ts/features/auth/api/register.ts
+++ resources/ts/features/auth/api/register.ts
@@ -1,28 +1,24 @@
-import { http } from "@/lib/http";
-import { AuthUser } from "../types";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useAuthUserStore } from "@/store/authUserStore";
-import { authKeys } from "@/features/auth/api/queryKey";
+import { http } from '@/lib/http'
+import { AuthUser } from '../types'
+import { useMutation } from '@tanstack/react-query'
 
 export type RegisterCredentials = {
-  email: string;
-  name: string;
-  password: string;
-};
-export const registerWithEmailAndPassword = (
-  data: RegisterCredentials
-): Promise<AuthUser> => {
-  return http.post("/api/register", data);
-};
+  email: string
+  name: string
+  password: string
+}
+
+export type RegisterErrorResponse = {
+  response: {
+    data: string | string[]
+  }
+}
+export const registerWithEmailAndPassword = (data: RegisterCredentials): Promise<AuthUser> => {
+  return http.post('/api/register', data)
+}
 
 export const useRegister = () => {
-  const queryClient = useQueryClient();
-  const loginUser = useAuthUserStore((state) => state.login);
-  return useMutation({
-    onSuccess: (res: AuthUser) => {
-      queryClient.setQueryData(authKeys.user, res);
-      loginUser(res);
-    },
+  return useMutation<AuthUser, RegisterErrorResponse, RegisterCredentials>({
     mutationFn: registerWithEmailAndPassword,
-  });
-};
+  })
+}
